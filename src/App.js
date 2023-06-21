445ff6ca968dc60563ca23939c504238b164906c
badnanna <badnanna@emeal.nttdata.com>
2023-06-21 20:18:56 +0100

    basic counter to learn react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(() => {
+    console.log("start")
+    return 0
+  })
+  function increment() {
+      setCount((previousCount) => previousCount + 1)
+  }
+  function decrement() {
+      setCount((previousCount) => previousCount - 1)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={increment}>+</button>
+      <span>{count}</span>
+      <button onClick={decrement}>-</button>
+      
+    </>
   );
 }
 
